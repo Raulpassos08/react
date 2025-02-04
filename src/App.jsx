879e1a92f405d94c168812bb2bf2303a015caa9a
Raulpassos08 <raulfera2@gmail.com>
2025-02-04 14:41:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const x = 10
+  const y = 15
+
+  const resultado = x + y
+  return (
+    <div>
+      <p>{resultado}</p>
+      <button>Adicionar</button>
+    </div>
+  )
+}
+
+export default App
